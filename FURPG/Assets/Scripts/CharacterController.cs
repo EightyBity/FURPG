99a27be249ee8c4ecf4f9a25f8c3e5174db9b01a
Eighty_Bity <byavik2@gmail.com>
2023-08-11 21:39:36 -0400

    added BASIC movements, will need rework

--- FURPG/Assets/Scripts/CharacterController.cs
+++ FURPG/Assets/Scripts/CharacterController.cs
@@ -7,6 +7,7 @@ public class CharacterController : MonoBehaviour
     public float speed = 10f;
 
     public Rigidbody2D rb;
+    Vector2 movement;
     
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,17 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxisRaw("Horizontal")
+        Input.GetAxisRaw("Horizontal");
+
+        Input.GetAxisRaw("Vertical");
+        
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
